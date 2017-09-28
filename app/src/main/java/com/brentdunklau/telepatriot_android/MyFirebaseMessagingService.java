ba49e83868cd039b97de83255b4331f61dcd23f1
bdunklau <bdunklau@yahoo.com>
2017-09-28 18:13:08 -0500

    getting close to having notifications display on the phone.  See MyFirebaseMessagingService

--- app/src/main/java/com/brentdunklau/telepatriot_android/MyFirebaseMessagingService.java
+++ app/src/main/java/com/brentdunklau/telepatriot_android/MyFirebaseMessagingService.java
@@ -1,6 +1,14 @@
 package com.brentdunklau.telepatriot_android;
 
+import java.util.Map;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -14,13 +22,53 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     public MyFirebaseMessagingService() {
     }
 
+    /**
+     *
+     * @see  https://github.com/firebase/quickstart-android/blob/master/messaging/app/src/main/java/com/google/firebase/quickstart/fcm/MyFirebaseMessagingService.java#L45-L82
+     *
+     */
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         // Handle data payload of FCM messages.
         Log.d(TAG, "FCM Message Id: " + remoteMessage.getMessageId());
         Log.d(TAG, "FCM Notification Message: " +
                 remoteMessage.getNotification());
+        Map<String, String> data = remoteMessage.getData();
         Log.d(TAG, "FCM Data Message: " + remoteMessage.getData());
+
+        sendNotification(data.get("message"), data.get("title"));
+    }
+
+
+
+    /**
+     * Create and show a simple notification containing the received FCM message.
+     *
+     * @see  https://github.com/firebase/quickstart-android/blob/master/messaging/app/src/main/java/com/google/firebase/quickstart/fcm/MyFirebaseMessagingService.java#L45-L82
+     *
+     * @param messageBody FCM message body received.
+     */
+    private void sendNotification(String messageBody, String title) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        String channelId = getString(R.string.default_notification_channel_id);
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, channelId)
+                        .setSmallIcon(R.drawable.usflag)  // change this to something else
+                        .setContentTitle(title)
+                        .setContentText(messageBody)
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
 
 }
