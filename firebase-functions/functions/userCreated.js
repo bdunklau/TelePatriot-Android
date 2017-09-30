9cd3e58cc4486f875e5a6e5b9ddd1c6b91aa7e94
Brent Dunklau <bdunklau@yahoo.com>
2017-09-29 21:02:12 -0500

    Created a trigger in userCreated.js that fires whenever a new user is created.  The trigger writes the user's email address and photourl to the /users node.

---
+++ firebase-functions/functions/userCreated.js
@@ -0,0 +1,24 @@
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+
+// create reference to root of the database
+const ref = admin.database().ref()
+
+exports.createUserAccount = functions.auth.user().onCreate(event => {
+    console.log("userCreated.js: onCreate called")
+    // UserRecord is created
+    // according to: https://www.youtube.com/watch?v=pADTJA3BoxE&t=31s
+    // UserRecord contains: displayName, email, photoUrl, uid
+    // all of this is accessible via event.data
+    const uid = event.data.uid
+    const email = event.data.email
+    const photoUrl = event.data.photoUrl || 'https://i.stack.imgur.com/34AD2.jpg'
+
+    // apparently you have to use backticks, not single quotes
+    const newUserRef = ref.child(`/users/${uid}`)
+
+    console.log("userCreated.js: onCreate returning...")
+
+    // remember, .set() returns a promise
+    return newUserRef.set({photoUrl:photoUrl, email:email})
+})
