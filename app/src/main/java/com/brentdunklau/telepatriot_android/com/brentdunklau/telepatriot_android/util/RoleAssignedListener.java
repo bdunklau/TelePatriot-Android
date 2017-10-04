5752d342ebc10dd3cb0a5c8d6dfa44fed6f52d2e
Brent Dunklau <bdunklau@yahoo.com>
2017-10-04 02:14:50 -0500

    Made LimboActivity aware when the first role is assigned to a user, and when it is, we immediate send that user to the screen matching that role - i.e.  Admin -> AdminActivity

---
+++ app/src/main/java/com/brentdunklau/telepatriot_android/com/brentdunklau/telepatriot_android/util/RoleAssignedListener.java
@@ -0,0 +1,10 @@
+package com.brentdunklau.telepatriot_android.com.brentdunklau.telepatriot_android.util;
+
+/**
+ * Created by bdunklau on 10/4/17.
+ */
+
+public interface RoleAssignedListener {
+
+    void roleAssigned(String role);
+}
