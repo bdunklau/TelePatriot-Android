e6466c821aa3970f5ba042807efbe513def1f26c
bdunklau <bdunklau@yahoo.com>
2017-10-11 12:04:45 -0500

    Added Chat/Help and Sign Out to the nav drawer, even though they don't do anything yet

--- app/src/main/java/com/brentdunklau/telepatriot_android/MainActivity.java
+++ app/src/main/java/com/brentdunklau/telepatriot_android/MainActivity.java
@@ -1,11 +1,6 @@
 package com.brentdunklau.telepatriot_android;
 
-import android.app.Fragment;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.FragmentManager;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -14,11 +9,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -50,7 +40,7 @@ public class MainActivity extends AppCompatActivity
         });
         */
 
-        
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -107,9 +97,9 @@ public class MainActivity extends AppCompatActivity
             fragmentManager.beginTransaction().replace(R.id.content_frame, new DirectorFragment()).commit();
         } else if (id == R.id.nav_admin_layout) {
             fragmentManager.beginTransaction().replace(R.id.content_frame, new AdminFragment()).commit();
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_chat) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_signout) {
 
         }
 
