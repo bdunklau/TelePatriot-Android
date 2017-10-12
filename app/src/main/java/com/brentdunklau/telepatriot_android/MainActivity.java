e47f4ba49925724b92d6656fbcd870471c8d5e5c
bdunklau <bdunklau@yahoo.com>
2017-10-12 16:35:26 -0500

    Make it so the nav drawer opens when the user first logs in.  Otherwise we would have to give them some indication that the nav drawer even exists.  And the main screen is just blank without it.

--- app/src/main/java/com/brentdunklau/telepatriot_android/MainActivity.java
+++ app/src/main/java/com/brentdunklau/telepatriot_android/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -74,6 +75,7 @@ public class MainActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
+        drawer.openDrawer(Gravity.START);
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
