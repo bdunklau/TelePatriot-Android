fc93bab75387d934defe91667db3fda1df87c3da
Brent Dunklau <bdunklau@yahoo.com>
2017-10-16 22:54:43 -0500

    test commit so I can test merging master into this branch (instead of the other way around which is what I usually do)

--- app/src/main/java/com/brentdunklau/telepatriot_android/DirectorFragment.java
+++ app/src/main/java/com/brentdunklau/telepatriot_android/DirectorFragment.java
@@ -29,7 +29,7 @@ public class DirectorFragment extends Fragment {
         btnNewPhoneCampaign.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                gotoNewPhoneCampaignFragment();
+                gotoNewPhoneCampaignFragment(); 
             }
         });
 
