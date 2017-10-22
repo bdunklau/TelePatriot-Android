65083b45b64ff52cad1faf26e232ba53c4156a04
Brent Dunklau <bdunklau@yahoo.com>
2017-10-21 20:31:28 -0500

    Conf Calling! We can actually do a conference call with 3 other people which is what we were hoping for.

---
+++ app/src/main/java/com/brentdunklau/telepatriot_android/PhoneCallActivity.java
@@ -0,0 +1,10 @@
+package com.brentdunklau.telepatriot_android;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by bdunklau on 10/21/17.
+ */
+
+public class PhoneCallActivity extends AppCompatActivity {
+}
