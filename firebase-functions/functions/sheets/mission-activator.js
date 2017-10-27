e6e47faec3d7080d7e6c421b6c3cbec33fb55b4c
Brent Dunklau <bdunklau@yahoo.com>
2017-10-27 08:42:26 -0500

    not sure - work in progress

--- firebase-functions/functions/sheets/mission-activator.js
+++ firebase-functions/functions/sheets/mission-activator.js
@@ -7,6 +7,21 @@ const functions = require('firebase-functions');
 exports.missionActivation = functions.database.ref(`missions/{missionId}/{uid_and_active}`).onWrite(
     event => {
 
+    // This keeps your trigger from being called recursively
+    // In this case, we want to only execute this trigger when the uid_and_active attribute
+    // already exists
+    if (event.data.previous.exists()) {
+        return;
+    }
+    // Exit when the data is deleted.
+    if (!event.data.exists()) {
+        return;
+    }
+
+    if(event.data.previous.val().uid_and_active == event.data.val().uid_and_active)
+        return // if there's no change to this value
+
+
     var missionId = event.params.missionId
     var uid_and_active = event.params.uid_and_active
     var active = uid_and_active.endsWith("_true")
@@ -17,6 +32,6 @@ exports.missionActivation = functions.database.ref(`missions/{missionId}/{uid_an
         updates["mission_details/"+snapshot.key+"/active"] = active;
         updates["mission_details/"+snapshot.key+"/uid_and_active"] = uid_and_active;
     });
-    return adminRef.update(updates);
+    return adminRef.root.update(updates);
 
 })
