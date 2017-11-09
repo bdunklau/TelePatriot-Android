77744ab16a3b339b82cd1e170907aa7515dff8a8
Brent Dunklau <bdunklau@yahoo.com>
2017-11-08 23:35:52 -0600

    Replaced the VolunteerFragment and the Volunteer View (in activity_main_drawer.xml) with "Get Your Mission".  There's no reason to make the user click "Volunteer View" and *then* "Get Your Mission".  Keep it simple and get rid of clicks wherever possible

--- app/src/main/java/com/brentdunklau/telepatriot_android/MainActivity.java
+++ app/src/main/java/com/brentdunklau/telepatriot_android/MainActivity.java
@@ -138,7 +138,7 @@ public class MainActivity extends AppCompatActivity
 
 
         if (id == R.id.nav_volunteer_layout) {
-            fragmentManager.beginTransaction().replace(R.id.content_frame, new VolunteerFragment()).commit();
+            fragmentManager.beginTransaction().replace(R.id.content_frame, new GetAMissionFragment()).commit();
         } else if (id == R.id.nav_director_layout) {
             fragmentManager.beginTransaction().replace(R.id.content_frame, new DirectorFragment()).commit();
         } else if (id == R.id.nav_admin_layout) {
