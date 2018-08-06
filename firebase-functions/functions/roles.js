6708864a5fc0da3dde1216d7aef2c01fc3b03680
bdunklau <bdunklau@yahoo.com>
2018-08-06 09:19:48 -0500

    fixing roleAssigned

--- firebase-functions/functions/roles.js
+++ firebase-functions/functions/roles.js
@@ -7,41 +7,30 @@ const date = require('./dateformat')
 
 // TODO fix this
 /* Listens for new messages added to __________ and then ____________ */
-exports.roleAssigned = functions.database.ref('/users/{uid}/roles/{role}').onWrite( event => {
-    var evtsnapshot = event.data
+exports.roleAssigned = functions.database.ref('/users/{uid}/roles/{role}').onCreate( event => {
     console.log("roleAssigned: uid=", event.params.uid, "role=", event.params.role)
-
-    // Only edit data when it is first created.
-    if (event.data.previous.exists()) {
-        return;
-    }
-    // Exit when the data is deleted.
-    if (!event.data.exists()) {
-        return;
-    }
 
     // event.params.role will be the value of the key so make the key 'Admin' and the value doesn't matter
     var uid = event.params.uid
     var role = event.params.role
 
-    event.data.adminRef.root.child(`/users/${uid}`).once('value').then(snapshot => {
-        console.log("snapshot.val(): ", snapshot.val())
-        event.data.adminRef.root.child(`/roles/${role}/users/${uid}`)
+    event.data.adminRef.root.child('/users/'+uid).once('value').then(snapshot => {
+        event.data.adminRef.root.child('/roles/'+role+'/users/'+uid)
             .set({name: snapshot.val().name, email: snapshot.val().email})
     })
 
 
     // now need to get the topics associated with this role
-    return event.data.adminRef.root.child(`/roles/${role}/topics`).once('value').then(snapshot => {
+    return event.data.adminRef.root.child('roles/'+role+'/topics').once('value').then(snapshot => {
           snapshot.forEach(function(child) {
             console.log("child.val(): ", child.val())
             var topic = child.val().name
-            event.data.adminRef.root.child(`/users/${uid}/topics`).child(topic).set('true')
+            event.data.adminRef.root.child('/users/'+uid+'/topics/'+topic).set('true')
           });
     }).then(snapshot => {
         var datestr = date.asCentralTime()
         var msg = "Admin has assigned you to the "+role+" group"
-        event.data.adminRef.root.child(`/users/${uid}/account_status_events`).push({date: datestr, event: msg})
+        return event.data.adminRef.root.child('/users/'+uid+'/account_status_events').push({date: datestr, event: msg})
     })
 
 });
@@ -50,8 +39,6 @@ exports.roleAssigned = functions.database.ref('/users/{uid}/roles/{role}').onWri
 
 exports.roleUnassigned = functions.database.ref('/users/{uid}/roles/{role}').onDelete( event => {
     console.log("roleUnassigned: uid=", event.params.uid, "role=", event.params.role)
-    // this is the DatabaseSnapshot object
-    //var evtsnapshot = event.data.previous
 
     var role = event.params.role
     var uid = event.params.uid
