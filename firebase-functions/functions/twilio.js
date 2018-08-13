6194a7c3fbd441ac88b5c5cda6d54cf6180dc54f
bdunklau <bdunklau@yahoo.com>
2018-08-13 16:25:20 -0500

    twilio.js:testListRooms() - need callback function, not callback url

--- firebase-functions/functions/twilio.js
+++ firebase-functions/functions/twilio.js
@@ -88,12 +88,8 @@ exports.testListRooms = functions.https.onRequest((req, res) => {
         const client = twilio(snapshot.val().twilio_account_sid, snapshot.val().twilio_auth_token)
 
         client.video.rooms
-                    .each({ /*see https://www.twilio.com/docs/video/api/rooms-resource#get-list-resource for parms*/
-                        status_callback: 'https://'+req.get('host')+'/twilioCallback'
-                    })
-                    .then(rooms => {
-                        console.log(room.sid)
-                        db.ref('templog2').set({room: room})
+                    .each({ /*see https://www.twilio.com/docs/video/api/rooms-resource#get-list-resource for parms*/ },
+                    function(rooms) {
                         var html = ''
                         html += '<html><head></head><body>'
                         html += '<table>'
