f4d3a58b9711661cd4ae1ff24a34edcbbb564705
bdunklau <bdunklau@yahoo.com>
2018-09-21 10:31:23 -0500

    creating a test web front end for /volunteers

--- firebase-functions/functions/citizen_builder_api/volunteers.js
+++ firebase-functions/functions/citizen_builder_api/volunteers.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// lodash dependency declared in firebase-functions/functions/package.json
+const _ = require('lodash');
 const admin = require('firebase-admin')
 const functions = require('firebase-functions')
 const date = require('../dateformat')
