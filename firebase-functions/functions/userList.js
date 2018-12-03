5cc6c9fb543ca6c3efe6ffc0d62aa14546028db4
Brent Dunklau <bdunklau@yahoo.com>
2018-12-03 10:34:30 -0600

    Big Milestone:  Can cycle through one call after another on Android and the app will even recognize when there's no one left to call.

--- firebase-functions/functions/userList.js
+++ firebase-functions/functions/userList.js
@@ -52,7 +52,9 @@ var listUsers = function(stuff) {
                 phone = child.val().phone
             }
             var roles = []
-            var auser = {uid: child.key, name: child.val().name, email: child.val().email, phone: phone}
+            var auser = {uid: child.key, name: child.val().name, email: child.val().email, phone: phone,
+                        has_signed_petition: child.val().has_signed_petition, has_signed_confidentiality_agreement: child.val().has_signed_confidentiality_agreement,
+                        is_banned: child.val().is_banned}
             var hasRole /*we're looking for*/ = false
             if(child.val().roles) {
                 _.each(Object.keys(child.val().roles), function(role) {
@@ -78,7 +80,7 @@ var listUsersAsHtml = function(stuff) {
     var stuff = ''
     stuff += '<table>'
     stuff +=    '<tr>'
-    stuff +=        '<td colspan="5">'
+    stuff +=        '<td colspan="8">'
     stuff +=            '<b> <a href="/downloadUsers'+roleParm+'">Download</a> All Users</b> &nbsp;&nbsp;&nbsp;&nbsp;'
     stuff +=            '<a href="/manageUsers?role=Admin">Admin</a> &nbsp;&nbsp;&nbsp;&nbsp;'
     stuff +=            '<a href="/manageUsers?role=Director">Director</a> &nbsp;&nbsp;&nbsp;&nbsp;'
@@ -89,15 +91,28 @@ var listUsersAsHtml = function(stuff) {
     stuff += '<tr>'
     stuff += '<th style="'+tableheading+'">Name</th>'
     stuff += '<th style="'+tableheading+'">Email</th>'
+    stuff += '<th style="'+tableheading+'">Petition</th>'
+    stuff += '<th style="'+tableheading+'">CA</th>'
+    stuff += '<th style="'+tableheading+'">Banned</th>'
     stuff += '<th style="'+tableheading+'">Phone</th>'
-    stuff += '<th style="'+tableheading+'"></th>'
+    stuff += '<th style="'+tableheading+'">Save</th>'
     stuff += '<th style="'+tableheading+'">Roles</th>'
     stuff += '</tr>'
     for(var i=0; i < users.length; i++) {
+        var pcolor = color(users[i].has_signed_petition, true)
+        var ccolor = color(users[i].has_signed_confidentiality_agreement, true)
+        var bcolor = color(users[i].is_banned, false)
+        var pstyle = style+";background-color:"+pcolor
+        var cstyle = style+";background-color:"+ccolor
+        var bstyle = style+";background-color:"+bcolor
+
         stuff += '<tr>'
         stuff += '<form method="post" action="/updateUser"><input type="hidden" name="uid" value="'+users[i].uid+'"/>'
         stuff += '<td style="'+style+'" valign="top">'+users[i].name+'</td>'
         stuff += '<td style="'+style+'" valign="top">'+users[i].email+'</td>'
+        stuff += '<td style="'+pstyle+'" valign="top">'+users[i].has_signed_petition+'</td>'
+        stuff += '<td style="'+cstyle+'" valign="top">'+users[i].has_signed_confidentiality_agreement+'</td>'
+        stuff += '<td style="'+bstyle+'" valign="top">'+users[i].is_banned+'</td>'
         stuff += '<td style="'+style+'" valign="top"><input type="text" name="phone" placeholder="phone" size="15" value="'+users[i].phone+'"/></td>'
         stuff += '<td style="'+style+'"><input type="submit" value="save"></td>'
         stuff += '<td style="'+style+'" valign="top">'+users[i].roles+'</td>'
@@ -106,6 +121,16 @@ var listUsersAsHtml = function(stuff) {
     }
     stuff += '</table>'
     return stuff
+}
+
+
+var color = function(val, goodVal) {
+    var good = "#008B00"
+    var bad = "#e5125d"
+    var neutral = "#ffffff"
+    if(val == null) return neutral
+    else if(val == goodVal) return good
+    else return bad
 }
 
 
